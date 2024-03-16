e52d0fd88e215cad723c7321f81727d9a2896d09
Ian <iandkaneshiro@gmail.com>
2024-03-15 20:13:17 -0700

    solution

---
+++ 3_15_24#4.js
@@ -0,0 +1,33 @@
+/*Good morning! Here's your coding interview problem for today.
+
+This problem was asked by Stripe.
+
+Given an array of integers, find the first missing positive integer in linear time and constant space. In other words, find the lowest positive integer that does not exist in the array. The array can contain duplicates and negative numbers as well.
+
+For example, the input [3, 4, -1, 1] should give 2. The input [1, 2, 0] should give 3.
+
+You can modify the input array in-place.*/
+
+const sampleInput = [3, 4, -1, 1];
+const sampleInput2 = [1, 2, 0];
+
+const firstMissingPositiveInterger = (nums) => {
+  const n = nums.length;
+
+  for (let i = 0; i < n; i++) {
+    while (nums[i] > 0 && nums[i] <= n && nums[nums[i] - 1] !== nums[i]) {
+      [nums[nums[i] - 1], nums[i]] = [nums[i], nums[nums[i] - 1]];
+    }
+  }
+
+  for (let i = 0; i < n; i++) {
+    if (nums[i] !== i + 1) {
+      return i + 1;
+    }
+  }
+
+  return n + 1;
+};
+
+console.log(firstMissingPositiveInterger(sampleInput)); // 2
+console.log(firstMissingPositiveInterger(sampleInput2)); // 3
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # dailyCodeProblems
+
+Daily emails I get from Daily Coding Problems and my solutions
